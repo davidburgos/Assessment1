86029a8b623662e08ff1e661b79bdc84d3067fea
DavidBurgos <david.burgos@globant.com>
2015-02-10 21:09:06 -0500

    Initial Commit.
    -create an empty project with an activity with a fragment

---
+++ app/src/main/java/co/mobilemakers/picoyplaca/mainFragment.java
@@ -0,0 +1,22 @@
+package co.mobilemakers.picoyplaca;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class mainFragment extends Fragment {
+
+    public mainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
